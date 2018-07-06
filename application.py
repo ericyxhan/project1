e397558b571d16dde458442b78d62ef30b05cd29
Eric Han <ericyxhan@gmail.com>
2018-07-06 13:44:37 +0000

    coolio

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, render_template, session, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+@app.route("/pictures.html")
+def pictures():
+    return render_template("pictures.html")
+@app.route("/updates.html")
+def updates():
+    return render_template("updates.html")
